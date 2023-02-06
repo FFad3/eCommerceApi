b1613c4106a40d3e106008a98b9e727d5576f90d
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-06 22:53:04 +0100

    Add mediatR and autoMapper

---
+++ eCommerceApp.Application/ApplicationServicesRegistration.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace eCommerceApp.Application
+{
+    public static class ApplicationServicesRegistration
+    {
+        public static IServiceCollection IServiceCollection(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
