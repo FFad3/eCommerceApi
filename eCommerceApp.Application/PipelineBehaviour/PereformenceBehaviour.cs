823cb32d3ed3dfe98d6f2ec7cdcd5846fc70be14
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-07 15:38:01 +0100

    Add Pipeline Pereformence Behaviour

---
+++ eCommerceApp.Application/PipelineBehaviour/PereformenceBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceApp.Application.PipelineBehaviour
+{
+    internal sealed class PereformenceBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly Stopwatch _timer;
+        private readonly ILogger<TRequest> _logger;
+
+        public PereformenceBehaviour(ILogger<TRequest> logger)
+        {
+            _timer = new Stopwatch();
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            _timer.Start();
+            var response = await next();
+            _timer.Stop();
+
+            var elapsedMilliseconds = _timer.Elapsed;
+            var requestName = typeof(TRequest).Name;
+            _logger.LogInformation("{request} | Execution time: {time}", requestName, elapsedMilliseconds);
+            return response;
+        }
+    }
+}
