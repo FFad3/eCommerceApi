456528de34ec04dcba12952c83f2c88a4f73b3d5
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-07 02:03:59 +0100

    Add custom exception

---
+++ eCommerceApp.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,20 @@
+﻿namespace eCommerceApp.Application.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException(string name, object key) : base($"{name} ({key}) was not found")
+    {
+    }
+
+    public NotFoundException() : base()
+    {
+    }
+
+    public NotFoundException(string? message) : base(message)
+    {
+    }
+
+    public NotFoundException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
