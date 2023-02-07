1ef0ea263b12d0da113e7475d6a540f88d58f126
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-07 03:10:39 +0100

    Update Category mapping profile

--- eCommerceApp.Application/MappingProfiles/CategoryProfile.cs
+++ eCommerceApp.Application/MappingProfiles/CategoryProfile.cs
@@ -1,5 +1,8 @@
 ﻿using AutoMapper;
 using eCommerceApp.Application.Features.Category.Commands.CreateCategory;
+using eCommerceApp.Application.Features.Category.Commands.UpdateCategory;
+using eCommerceApp.Application.Features.Category.Queries.GetAllCategories;
+using eCommerceApp.Application.Features.Category.Queries.GetSingleCategory;
 
 namespace eCommerceApp.Application.MappingProfiles
 {
@@ -8,6 +11,9 @@ namespace eCommerceApp.Application.MappingProfiles
         public CategoryProfile()
         {
             CreateMap<CreateCategoryCommand, Domain.Category>();
+            CreateMap<UpdateCategoryCommand, Domain.Category>();
+            CreateMap<Domain.Category, CategoryDto>();
+            CreateMap<Domain.Category, CategoryWithProductsDto>();
         }
     }
 }
