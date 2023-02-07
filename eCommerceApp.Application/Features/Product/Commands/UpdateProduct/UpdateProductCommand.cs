ac8669da734c58adf32a84502932a99c304ece14
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-07 15:40:16 +0100

    Add Update Product Command && Validator && Handler

---
+++ eCommerceApp.Application/Features/Product/Commands/UpdateProduct/UpdateProductCommand.cs
@@ -0,0 +1,18 @@
+﻿using eCommerceApp.Application.Features.Product.Commands.CreateProduct;
+using MediatR;
+
+namespace eCommerceApp.Application.Features.Product.Commands.UpdateProduct
+{
+    public class UpdateProductCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; } = string.Empty;
+        public string ImgUrl { get; set; } = string.Empty;
+    }
+}
---
+++ eCommerceApp.Application/Features/Product/Commands/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using eCommerceApp.Application.Contracts.Persistence;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace eCommerceApp.Application.Features.Product.Commands.UpdateProduct
+{
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, int>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+
+        public UpdateProductCommandHandler(IUnitOfWork unitOfWork, IMapper mapper, ILogger logger)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<int> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var productToCreate = _mapper.Map<Domain.Product>(request);
+
+            _unitOfWork.Product.Update(productToCreate);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return request.Id;
+        }
+    }
+}
