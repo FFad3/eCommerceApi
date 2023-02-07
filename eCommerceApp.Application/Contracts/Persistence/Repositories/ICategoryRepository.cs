fe13882eedab59fc73d59a3ae4f9efe1520dc31d
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-07 03:09:06 +0100

    +

--- eCommerceApp.Application/Contracts/Persistence/Repositories/ICategoryRepository.cs
+++ eCommerceApp.Application/Contracts/Persistence/Repositories/ICategoryRepository.cs
@@ -1,8 +1,10 @@
 ﻿using eCommerceApp.Domain;
+using System.Linq.Expressions;
 
 namespace eCommerceApp.Application.Contracts.Persistence
 {
     public interface ICategoryRepository : IGenericRepository<Category>
     {
+        Task<Category?> FindSingleWithProductsAsync(Expression<Func<Category, bool>> predicate, CancellationToken token);
     }
 }
