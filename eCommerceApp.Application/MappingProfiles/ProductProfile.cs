ff7b424baf144c1f982de727c035471204651f64
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-07 15:40:47 +0100

    Add Product mapping profile

---
+++ eCommerceApp.Application/MappingProfiles/ProductProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using eCommerceApp.Application.Features.Product.Commands.CreateProduct;
+using eCommerceApp.Application.Features.Product.Commands.UpdateProduct;
+
+namespace eCommerceApp.Application.MappingProfiles
+{
+    public class ProductProfile : Profile
+    {
+        public ProductProfile()
+        {
+            CreateMap<CreateProductCommand, Domain.Product>();
+            CreateMap<UpdateProductCommand, Domain.Product>();
+        }
+    }
+}
