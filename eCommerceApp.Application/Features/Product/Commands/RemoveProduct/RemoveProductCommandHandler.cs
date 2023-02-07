0a3e2d86874f09843a3c32fa1fd356a5bf0d8bac
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-07 15:40:34 +0100

    Add Remove Product Command && Handler

---
+++ eCommerceApp.Application/Features/Product/Commands/RemoveProduct/RemoveProductCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using eCommerceApp.Application.Contracts.Persistence;
+using eCommerceApp.Application.Exceptions;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace eCommerceApp.Application.Features.Product.Commands.RemoveProduct
+{
+    public class RemoveProductCommandHandler : IRequestHandler<RemoveProductCommand, Unit>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly ILogger _logger;
+
+        public RemoveProductCommandHandler(IUnitOfWork unitOfWork, IMapper mapper, ILogger logger)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<Unit> Handle(RemoveProductCommand request, CancellationToken cancellationToken)
+        {
+            var productToRemove = await _unitOfWork.Product.FindSingleAsync(x => x.Id == request.Id, cancellationToken);
+
+            if (productToRemove == null)
+                throw new NotFoundException(nameof(Domain.Category), request.Id);
+
+            _unitOfWork.Product.Remove(productToRemove);
+
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
