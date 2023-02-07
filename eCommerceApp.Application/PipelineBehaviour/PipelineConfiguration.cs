c1844edcde0f9cb3e39b41b00b80575e6bad28d9
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-07 15:38:28 +0100

    Add PipelineConfiguration

---
+++ eCommerceApp.Application/PipelineBehaviour/PipelineConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerceApp.Application.PipelineBehaviour
+{
+    internal static class PipelineConfiguration
+    {
+        internal static IServiceCollection ConfigureMediatRPipeline(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PereformenceBehaviour<,>));
+            return services;
+        }
+    }
+}
