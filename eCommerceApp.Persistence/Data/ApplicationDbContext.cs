f97b20be31670055b6d7e92ad7c422b370d287c2
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-12 15:02:21 +0100

    Add DbContext

---
+++ eCommerceApp.Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using eCommerceApp.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace eCommerceApp.Persistence.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+            configurationBuilder.Properties<string>().HaveColumnType("nvarchar(200)");
+            configurationBuilder.Properties<decimal>().HavePrecision(14, 2);
+        }
+
+        #region DbSets
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+        public DbSet<BasketItem> BasketItems { get; set; }
+
+        #endregion DbSets
+    }
+}
