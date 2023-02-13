353e26dc800851ba8d3b8b8d6aa4d4d344b7e4f8
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-13 18:23:45 +0100

    BasketRepository implementation

---
+++ eCommerceApp.Persistence/Repositories/BasketRepository.cs
@@ -0,0 +1,13 @@
+﻿using eCommerceApp.Application.Contracts.Persistence.Repositories;
+using eCommerceApp.Domain;
+using eCommerceApp.Persistence.Data;
+
+namespace eCommerceApp.Persistence.Repositories
+{
+    public class BasketRepository : GenericRepository<Basket>, IBasketRepository
+    {
+        public BasketRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
