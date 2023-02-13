8e2c5335c8f908052a85db1cdfb824121ee19992
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-13 18:23:33 +0100

    CateegoryRepository implementation

---
+++ eCommerceApp.Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using eCommerceApp.Application.Contracts.Persistence;
+using eCommerceApp.Domain;
+using eCommerceApp.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace eCommerceApp.Persistence.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        private readonly DbSet<Category> _categories;
+
+        public CategoryRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _categories = dbContext.Categories;
+        }
+
+        public Task<Category?> FindSingleWithProductsAsync(Expression<Func<Category, bool>> predicate, CancellationToken token)
+        {
+            return _categories.Include(x => x.Products.Where(p => !p.IsRemoved))
+                                .FirstOrDefaultAsync(predicate, token);
+        }
+    }
+}
