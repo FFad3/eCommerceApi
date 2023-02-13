e6b12bd8c1a32f8c4531d00a21c753aff7ab8e02
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-13 18:24:18 +0100

    Add UnitOfWork Implementation

---
+++ eCommerceApp.Persistence/UnitOfWork.cs
@@ -0,0 +1,56 @@
+﻿using eCommerceApp.Application.Contracts.Persistence;
+using eCommerceApp.Application.Contracts.Persistence.Repositories;
+using eCommerceApp.Persistence.Data;
+using eCommerceApp.Persistence.Repositories;
+
+namespace eCommerceApp.Persistence
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private bool _disposed = false;
+        private readonly ApplicationDbContext _dbContext;
+
+        #region Repositories
+
+        private readonly ICategoryRepository? _categoryRepository;
+        private readonly IProductRepository? _productRepository;
+        private readonly IBasketRepository? _basketRepository;
+        private readonly IOrderRepository? _orderRepository;
+
+        #endregion Repositories
+
+        public ICategoryRepository Category => _categoryRepository ?? new CategoryRepository(_dbContext);
+
+        public IProductRepository Product => _productRepository ?? new ProductRepository(_dbContext);
+
+        public IBasketRepository Basket => _basketRepository ?? new BasketRepository(_dbContext);
+
+        public IOrderRepository Order => _orderRepository ?? new OrderRepository(_dbContext);
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this._disposed && disposing)
+            {
+                _dbContext.Dispose();
+            }
+            this._disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task<bool> SaveChangesAsync(CancellationToken token)
+        {
+            //Mb return int to compare modified data count
+            return await _dbContext.SaveChangesAsync(token) > 0;
+        }
+    }
+}
