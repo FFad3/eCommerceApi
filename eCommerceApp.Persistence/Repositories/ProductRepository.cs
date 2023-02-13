c60572ab505375a6299ef700a5e891cde6c89c93
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-13 18:23:22 +0100

    ProductRepository implementation

---
+++ eCommerceApp.Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using eCommerceApp.Application.Contracts.Persistence.Repositories;
+using eCommerceApp.Domain;
+using eCommerceApp.Persistence.Data;
+
+namespace eCommerceApp.Persistence.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
