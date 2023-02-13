80c084173f948a0ee991ac3ec2ccd98099f6b858
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-13 18:43:45 +0100

    Add PersistanceServicesRegistration

---
+++ eCommerceApp.Persistence/PersistanceServicesRegistration.cs
@@ -0,0 +1,22 @@
+﻿using eCommerceApp.Application.Contracts.Persistence;
+using eCommerceApp.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerceApp.Persistence
+{
+    public static class PersistanceServicesRegistration
+    {
+        public static IServiceCollection RegisterPersistance(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("eCommerceDb"));
+            });
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            return services;
+        }
+    }
+}
