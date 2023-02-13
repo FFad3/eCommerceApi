b1164adc8318c6f175c08135f9725ff447f77913
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-13 18:23:56 +0100

    OrderRepository implementation

---
+++ eCommerceApp.Persistence/Repositories/OrderRepository.cs
@@ -0,0 +1,13 @@
+﻿using eCommerceApp.Application.Contracts.Persistence.Repositories;
+using eCommerceApp.Domain;
+using eCommerceApp.Persistence.Data;
+
+namespace eCommerceApp.Persistence.Repositories
+{
+    public class OrderRepository : GenericRepository<Order>, IOrderRepository
+    {
+        public OrderRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
