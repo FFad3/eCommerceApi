cfedd221b5af5958cab4acfc789efe3afb2eacc4
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-15 22:34:21 +0100

    ServicesRegistration

---
+++ eCommerce.Api/Configuration/ApiServicesRegistration.cs
@@ -0,0 +1,28 @@
+﻿using eCommerceApp.Application;
+using eCommerceApp.Infrastructure;
+using eCommerceApp.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Api.Configuration
+{
+    public static class ApiServicesRegistration
+    {
+        public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Add services to the container.
+            services.AddControllers();
+
+            services.AddSwagger();
+
+            services.RegisterApplication();
+            services.RegisterInfrastructure();
+            services.RegisterPersistance(configuration);
+
+            return services;
+        }
+    }
+}
---
+++ eCommerce.Api/Configuration/SwaggerConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace eCommerce.Api.Configuration
+{
+    public static class SwaggerConfiguration
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            return services;
+        }
+    }
+}
