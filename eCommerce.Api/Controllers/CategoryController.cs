d34fbb5c3fc36a8b7a0f279d0937a78fbc2e1649
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-19 14:56:32 +0100

    Add CategoryController

---
+++ eCommerce.Api/Controllers/CategoryController.cs
@@ -0,0 +1,49 @@
+﻿using eCommerceApp.Application.Features.Category.Commands.CreateCategory;
+using eCommerceApp.Application.Features.Category.Commands.UpdateCategory;
+using eCommerceApp.Application.Features.Category.Queries.GetAllCategories;
+using eCommerceApp.Application.Features.Category.Queries.GetSingleCategory;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CategoryController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _mediator.Send(new GetAllCategoriesQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _mediator.Send(new GetSingleCategoryQuery { Id = id });
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCategoryCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPatch]
+        public async Task<IActionResult> Update(UpdateCategoryCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
