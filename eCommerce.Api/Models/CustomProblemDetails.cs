3611d80d0a2e7a4a1e38103501c9de2008258e37
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-22 15:49:53 +0100

    Add CustomProblemDetails

---
+++ eCommerce.Api/Models/CustomProblemDetails.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Api.Models
+{
+    internal class CustomProblemDetails : ProblemDetails
+    {
+        public IDictionary<string, string[]> Errors { get; set; } = new Dictionary<string, string[]>();
+    }
+}
