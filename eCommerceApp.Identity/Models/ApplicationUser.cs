a523cfb8f75b40c6214df0fa05222909198e1135
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-27 16:33:08 +0100

    Add ApplicationUser

---
+++ eCommerceApp.Identity/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace eCommerceApp.Identity.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
