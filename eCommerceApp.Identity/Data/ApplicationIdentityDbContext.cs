c2de08b3d4cd7f6c6bc29fbe9fa8fc8deeb9a48f
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-27 16:33:40 +0100

    Add IdentityDbContext & EntitiesConfiguration

---
+++ eCommerceApp.Identity/Data/ApplicationIdentityDbContext.cs
@@ -0,0 +1,19 @@
+﻿using eCommerceApp.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceApp.Identity.Data
+{
+    public class ApplicationIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationIdentityDbContext).Assembly);
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ eCommerceApp.Identity/Data/IdentityEntityConfiguration.cs
@@ -0,0 +1,86 @@
+﻿using eCommerceApp.Identity.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eCommerceApp.Identity.Data
+{
+    public class IdentityRoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole
+                {
+                    Id = "001",
+                    Name = "Administrator",
+                    NormalizedName = "ADMINISTRATOR"
+                },
+                new IdentityRole
+                {
+                    Id = "002",
+                    Name = "Employee",
+                    NormalizedName = "EMPLOYEE"
+                },
+                new IdentityRole
+                {
+                    Id = "003",
+                    Name = "User",
+                    NormalizedName = "USER"
+                }
+                );
+        }
+    }
+
+    public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+        {
+            var hasher = new PasswordHasher<ApplicationUser>();
+            builder.HasData(
+                new ApplicationUser
+                {
+                    Id = "u001",
+                    Email = "admin@localhost.com",
+                    NormalizedEmail = "ADMIN@LOCALHOST.COM",
+                    FirstName = "System",
+                    LastName = "Admin",
+                    UserName = "admin@localhost.com",
+                    NormalizedUserName = "ADMIN@LOCALHOST.COM",
+                    PasswordHash = hasher.HashPassword(null!, "zaq1@WSX"),
+                    EmailConfirmed = true,
+                },
+                new ApplicationUser
+                {
+                    Id = "u002",
+                    Email = "user@localhost.com",
+                    NormalizedEmail = "USER@LOCALHOST.COM",
+                    FirstName = "System",
+                    LastName = "User",
+                    UserName = "user@localhost.com",
+                    NormalizedUserName = "USER@LOCALHOST.COM",
+                    PasswordHash = hasher.HashPassword(null!, "zaq1@WSX"),
+                    EmailConfirmed = true,
+                });
+        }
+    }
+
+    public class UserRoleConfiguration : IEntityTypeConfiguration<IdentityUserRole<string>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+        {
+            builder.HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = "001",
+                    UserId = "u001"
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = "003",
+                    UserId = "u002"
+                }
+                );
+        }
+    }
+}
