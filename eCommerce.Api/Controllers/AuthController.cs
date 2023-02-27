13f39489ff717824e778c1be38d31cf54a393581
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-27 16:36:02 +0100

    Add AuthController

---
+++ eCommerce.Api/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using eCommerceApp.Application.Contracts.Identity;
+using eCommerceApp.Application.Models.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponse>> Login(AuthRequest request)
+        {
+            return Ok(await _authService.Login(request));
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+        {
+            return Ok(await _authService.Register(request));
+        }
+    }
+}
