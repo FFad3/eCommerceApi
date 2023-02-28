ab251925d961520376b3f7255aadfd9d9c174850
Patryk Gneba <gnebapatryk@gmail.com>
2023-02-28 15:46:14 +0100

    Add eCOmmerce.UI - Angular

---
+++ eCommerce.UI/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public categories?: Category[];
+
+  constructor(http: HttpClient) {
+    http.get<Category[]>('https://localhost:7098/api/Category').subscribe(result => {
+      this.categories = result;
+    }, error => console.error(error));
+  }
+
+  title = 'eCommerce.UI';
+}
+
+interface Category {
+  id: string;
+  name: string;
+  productsCount: number;
+}
---
+++ eCommerce.UI/src/proxy.conf.js
@@ -0,0 +1,7 @@
+const PROXY_CONFIG = [
+  {
+    target: "https://localhost:7098",
+  }
+]
+
+module.exports = PROXY_CONFIG;
