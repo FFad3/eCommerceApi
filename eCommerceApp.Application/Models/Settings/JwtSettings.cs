49775e609f5ec311d3fb823cee8c77db739e6880
FFad3 <gnebapatryk@gmail.com>
2023-04-05 12:10:18 +0200

    add RefreshToken settings

--- eCommerceApp.Application/Models/Settings/JwtSettings.cs
+++ eCommerceApp.Application/Models/Settings/JwtSettings.cs
@@ -6,6 +6,7 @@
         public string Issuer { get; set; } = string.Empty;
         public string Audience { get; set; } = string.Empty;
         public double DurationInMinutes { get; set; }
+        public double RefreshTokenValidityInDays { get; set; }
         public DateTime ExpirationDate => DateTime.Now.AddMinutes(DurationInMinutes);
     }
 }
