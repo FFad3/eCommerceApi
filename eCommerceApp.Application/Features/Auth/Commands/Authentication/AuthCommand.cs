604eda3e1bb76b28474136e33a9499c12f0305c1
FFad3 <gnebapatryk@gmail.com>
2023-04-06 16:51:45 +0200

    Add AuthCommand&Handler&Validator

---
+++ eCommerceApp.Application/Features/Auth/Commands/Authentication/AuthCommand.cs
@@ -0,0 +1,11 @@
+﻿using eCommerceApp.Application.Models.Identity;
+using MediatR;
+
+namespace eCommerceApp.Application.Features.Auth.Commands.Authentication
+{
+    public class AuthCommand : IRequest<AuthResponse?>
+    {
+        public string Email { get; set; } = "user@localhost.com";
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ eCommerceApp.Application/Features/Auth/Commands/Authentication/AuthCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using eCommerceApp.Application.Contracts.Identity;
+using eCommerceApp.Application.Models.Identity;
+using MediatR;
+
+namespace eCommerceApp.Application.Features.Auth.Commands.Authentication
+{
+    public class AuthCommandHandler : IRequestHandler<AuthCommand, AuthResponse?>
+    {
+        private readonly IAuthService _authService;
+
+        public AuthCommandHandler(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public async Task<AuthResponse?> Handle(AuthCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _authService.Login(request);
+            return result;
+        }
+    }
+}
