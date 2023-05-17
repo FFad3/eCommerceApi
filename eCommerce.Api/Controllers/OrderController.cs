b76945618c0ec5dd5724444598d7c3cb15663204
FFad3 <gnebapatryk@gmail.com>
2023-05-17 21:49:07 +0200

    Add endpoint for GetUserOrders && GetUserOrderDetails

--- eCommerce.Api/Controllers/OrderController.cs
+++ eCommerce.Api/Controllers/OrderController.cs
@@ -1,4 +1,6 @@
 ﻿using eCommerceApp.Application.Features.Order.Commands.PlaceOrderCommand;
+using eCommerceApp.Application.Features.Order.Queries.GetUserOrderDetails;
+using eCommerceApp.Application.Features.Order.Queries.GetUserOrders;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -7,6 +9,7 @@ namespace eCommerce.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class OrderController : ControllerBase
     {
         private readonly IMediator _mediator;
@@ -16,11 +19,22 @@ namespace eCommerce.Api.Controllers
             _mediator = mediator;
         }
 
-        [HttpPost("confirm")]
-        [Authorize]
+        [HttpPost("confirm")]        
         public async Task<IActionResult> RemoveItem(PlaceOrderCommand command)
         {
             var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+        [HttpGet("orders")]
+        public async Task<IActionResult> GetUserOrderList()
+        {
+            var result = await _mediator.Send(new GetUserOrdersQuery());
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUserOrderDetails(int id)
+        {
+            var result = await _mediator.Send(new GetUserOrderDetailsQuery { Id=id});
             return Ok(result);
         }
     }
